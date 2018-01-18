ae28ff52eebecde2f249a9a2ddd8c7ad36b01f14
Aliaksandr_Zahorski <aliaksandr_zahorski@epam.com>
2018-01-18 12:20:20 +0300

    solution is added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = function groupBy(array) {
-    // your solution here
+module.exports = function groupBy(cb = (cb) => cb) {
+    const pre = this.map((d, i) => new Object( {[cb(d)]: d} ));
+    let result = new Set();
+    for (let i = 0; i < pre.length; i++) {
+      result.add(Number(Object.keys(pre[i])));
+    }
+
+    let obj = Object.create(null);
+    for (let item of result) {
+      obj[item] = pre.filter(d => d.hasOwnProperty(item)).map(d => d[item]);
+    }
+    return obj;
 }
